ee73acdf7368b17344fe38484d2c7f7bcb1e079c
循环展开 <xkgeng@outlook.com>
2021-07-05 16:29:07 +0800

    add gpio in and startup

---
+++ gpio_ctrl/share/gpio_in.py
@@ -0,0 +1,64 @@
+#!/usr/bin/env python
+#-*- coding:utf-8 -*-
+
+import rospy
+from rospy.core import rospyinfo
+from std_msgs.msg import Bool
+import Jetson.GPIO as GPIO
+
+class GpioIn:
+    def __init__(self):
+        self.pin = [16, 18]
+        self.none = None
+
+        self.value1 = self.none
+        self.value1_bat = self.none
+        self.value1_mode = 0
+
+        self.value2 = self.none
+        self.value2_bat = self.none
+        self.value2_mode = 0
+
+        self.mode = 0
+        self.start = 0
+        rospy.set_param('/mode', 0)
+        rospy.set_param('/start', 0)
+
+        GPIO.setmode(GPIO.BOARD)
+        GPIO.setwarnings(False)
+        GPIO.setup(self.pin, GPIO.IN)
+        rospy.Timer(rospy.Duration(0.05), self.time_callback)
+        rospy.spin()
+
+    def time_callback(self, event):
+        self.value1_bat = self.value1
+        self.value1 = GPIO.input(self.pin[0])
+        if self.value1 == GPIO.HIGH and self.value1_bat == GPIO.LOW:
+            self.value1_mode = 1
+        if self.value1_mode == 1 and self.value1 == GPIO.LOW and self.value1_bat == GPIO.HIGH:
+            self.value1_mode = 0
+            if(self.mode < 3):
+                self.mode = self.mode + 1
+            else:
+                self.mode = 0
+            rospy.set_param('/mode', self.mode)
+            # print("Button1 have been pressed")
+        
+        self.value2_bat = self.value2
+        self.value2 = GPIO.input(self.pin[1])
+        if self.value2 == GPIO.HIGH and self.value2_bat == GPIO.LOW:
+            self.value2_mode = 1
+        if self.value2_mode == 1 and self.value2 == GPIO.LOW and self.value2_bat == GPIO.HIGH:
+            self.value2_mode = 0
+            if(self.start < 1):
+                self.start = self.start + 1
+            else:
+                self.start = 0
+            rospy.set_param('/start', self.start)
+            # print("Button2 have been pressed")
+
+def main():
+    rospy.init_node("gpio_in", anonymous = True)
+    GpioIn()
+
+main()
--- gpio_ctrl/share/led_buzz_ctrl.py
+++ gpio_ctrl/share/led_buzz_ctrl.py
@@ -10,11 +10,14 @@ class LedBuzzCtrl:
     def __init__(self):
         self.led_sub = rospy.Subscriber("/gpio/led", Bool, callback = self.led_callback)
         self.buzz_sub = rospy.Subscriber("/gpio/buzz", Bool, callback = self.buzz_callback)
-        self.pin = [35, 38]
+        self.pin = [29, 31]
+        #self.pin = [7, 11]
 
         GPIO.setmode(GPIO.BOARD)
         GPIO.setwarnings(False)
         GPIO.setup(self.pin, GPIO.OUT, initial = GPIO.LOW)
+
+        self.startup()
 
         rospy.spin()
     
@@ -29,10 +32,25 @@ class LedBuzzCtrl:
             GPIO.output(self.pin[1], GPIO.HIGH)
         else:
             GPIO.output(self.pin[1], GPIO.LOW)
+    
+    def startup(self):
+        GPIO.output(self.pin[0], GPIO.HIGH)
+        time.sleep(0.3)
+        GPIO.output(self.pin[0], GPIO.LOW)
+        time.sleep(0.3)
+
+        GPIO.output(self.pin[0], GPIO.HIGH)
+        time.sleep(0.3)
+        GPIO.output(self.pin[0], GPIO.LOW)
+        time.sleep(0.3)
+
+        GPIO.output(self.pin[0], GPIO.HIGH)
+        time.sleep(0.3)
+        GPIO.output(self.pin[0], GPIO.LOW)
+        time.sleep(0.3)
 
 def main():
     rospy.init_node("gpio_ctrl", anonymous = True)
     GPIO_Ctrl = LedBuzzCtrl()
 
-if __name__ == "__main__":
-    main()
+main()
