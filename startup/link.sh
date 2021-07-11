a126fac2234f5ea25ae7b13674fbc895fce3e25e
循环展开 <xkgeng@outlook.com>
2021-07-11 10:28:37 +0800

    update

--- startup/link.sh
+++ startup/link.sh
@@ -1,11 +1,15 @@
 #!/bin/bash
-sleep 10
+sleep 15
 sudo chmod 777 /dev/ttyTHS0
-python gpio_init.py
+#python gpio_init.py
 
 source /opt/ros/melodic/setup.bash
 source /home/nvidia/Code/cartographer_ws/install_isolated/setup.bash
 source /home/nvidia/Code/catkin_ws/devel/setup.bash
 source /home/nvidia/Code/mavros_ws/devel/setup.bash
 
+export ROS_MASTER_URI=http://192.168.1.138:11311
+export ROS_HOSTNAME=192.168.1.138
+
+sleep 3
 roslaunch whud_union whud_union.launch
