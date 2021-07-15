49ce9734ce53bc31e9a7a77d745497f74371d839
bird1and1fish <1014693892@qq.com>
2021-07-15 13:57:34 +0800

    add visual pose and visual vel

--- mavros_plugins/whud_tf.cpp
+++ mavros_plugins/whud_tf.cpp
@@ -13,7 +13,7 @@ namespace mavros{
 
                     lidar_timer = mav_control_nh.createTimer(ros::Duration(0.1), &WhudTfPlugin::lidar_cb, this);
                     visual_pos_timer = mav_control_nh.createTimer(ros::Duration(0.1), &WhudTfPlugin::visual_pos_cb, this);
-                    visual_vel_timer = mav_control_nh.createTimer(ros::Duration(0.1), &WhudTfPlugin::visual_vel_cb, this);
+                    visual_vel_timer = mav_control_nh.createTimer(ros::Duration(0.02), &WhudTfPlugin::visual_vel_cb, this);
 
                     // tf_sub = mav_control_nh.subscribe("/tf", 1, &WhudTfPlugin::tf_cb, this);
                 }
@@ -63,15 +63,75 @@ namespace mavros{
                         msg.q[2] = tf_transform.getRotation().getAxis().getY();
                         msg.q[3] = tf_transform.getRotation().getAxis().getZ();
 
-                        UAS_FCU(m_uas)->send_message_ignore_drop(msg);
+                        //UAS_FCU(m_uas)->send_message_ignore_drop(msg);
                     }
                 }
 
                 void visual_pos_cb(const ros::TimerEvent &event)
-                {}
+                {
+                    if(!visual_pose_transform_exist)
+                    {
+                        if(tf_listener_.canTransform("/camera_odom", "/camera_link", ros::Time(0)))
+                            visual_pose_transform_exist = true;
+                        else
+                            visual_pose_transform_exist = false;
+                    }
+                    else
+                    {
+                        tf::StampedTransform tf_transform;
+                        try
+                        {
+                            tf_listener_.lookupTransform("/camera_odom", "/camera_link", ros::Time(0), tf_transform);
+                        }
+                        catch(tf::TransformException &ex)
+                        {
+                            visual_pose_transform_exist = false;
+                            ROS_WARN("%s", ex.what());
+                            return;
+                        }
+                        mavlink::common::msg::VISION_POSITION_ESTIMATE msg;
+                        msg.x = tf_transform.getOrigin().getX();
+                        msg.y = tf_transform.getOrigin().getY();
+                        msg.z = tf_transform.getOrigin().getZ();
+                        msg.q[0] = tf_transform.getRotation().getW();
+                        msg.q[1] = tf_transform.getRotation().getAxis().getX();
+                        msg.q[2] = tf_transform.getRotation().getAxis().getY();
+                        msg.q[3] = tf_transform.getRotation().getAxis().getZ();
+
+                        UAS_FCU(m_uas)->send_message_ignore_drop(msg);
+                    }                    
+                }
 
                 void visual_vel_cb(const ros::TimerEvent &event)
-                {}
+                {
+                    if(!visual_vel_transform_exist)
+                    {
+                        if(tf_listener_.canTransform("/camera_link", "/camera_odom", ros::Time(0)))
+                            visual_vel_transform_exist = true;
+                        else
+                            visual_vel_transform_exist = false;
+                    }
+                    else
+                    {
+                        geometry_msgs::Twist tf_twist;
+                        try
+                        {
+                            tf_listener_.lookupTwist("/camera_link", "/camera_odom", ros::Time(0), ros::Duration(0.1), tf_twist);
+                        }
+                        catch(tf::TransformException &ex)
+                        {
+                            visual_vel_transform_exist = false;
+                            ROS_WARN("%s", ex.what());
+                            return;
+                        }
+                        mavlink::common::msg::VISION_SPEED_ESTIMATE msg;
+                        msg.x = tf_twist.linear.x;
+                        msg.y = tf_twist.linear.y;
+                        msg.z = tf_twist.linear.z;
+
+                        //UAS_FCU(m_uas)->send_message_ignore_drop(msg);
+                    }
+                }
 
                 /*
                 void tf_cb(const tf::tfMessage::ConstPtr &tf)
